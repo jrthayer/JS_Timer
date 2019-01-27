7b0213c78a3893d873d9b1da5844515d4b4a162f
James Thayer <jrthayer@users.noreply.github.com>
2019-01-26 19:35:00 -0800

    Redesign timer functionality
    *result is visually the same but functionally no longer dependant on the accuracy of the timeout function. This results in a more accruacte timer and cleaner code in general.

--- timer.js
+++ timer.js
@@ -11,106 +11,125 @@ var tmrSubOptions = document.getElementById("tmrSubOptions");
 
 var adjustRepeat = null;
 var tmrLoop;
-var tmrState = false;
+var tmrRunning = false;
+var tmrAmount = 0;
+var tmrMaxs = [1000, 60, 60, 24];
+var tmrTimes = [0,0,0,0];
 
-function timer()
-{
-    if(tmrState){
-        var sec = Number(tmrSecDisplay.textContent) - 1;
-
-        if(sec > 0){
-            tmrSecDisplay.textContent = tmrSingleConvert(sec);
-        }
-        else if(sec === 0){
-            tmrSecDisplay.textContent = "00";
-
-            var min = Number(tmrMinDisplay.textContent);
-            var hr =  Number(tmrHrDisplay.textContent);
-
-            if(min === 0 && hr === 0) tmrFinished();
-        }
-        else{
-            var min = Number(tmrMinDisplay.textContent);
-            var hr =  Number(tmrHrDisplay.textContent);
-            
-            //need for starting state of 00:00:00
-            if(min === 0 && hr === 0){
-                tmrFinished();
-            }
-            else
-            {
-                if(min != 0){
-                    min -= 1;
-                    tmrMinDisplay.textContent = tmrSingleConvert(min);
-                    tmrSecDisplay.textContent = 59;
-                }
-                else{
-                    hr -= 1;
-                    tmrHrDisplay.textContent = tmrSingleConvert(hr);
-                    tmrMinDisplay.textContent = 59;
-                    tmrSecDisplay.textContent = 59;
-                }
-            }
-            
-        }
-        tmrLoop = setTimeout(timer, 1000);
-    }
-    else{
-        clearTimeout(tmrLoop);
-    }   
+//used when to start or continue timer
+function tmrStart(){
+    tmrRunning = true;
+    var curTime = Date.now();
+    tmrConvertToMS();
+    tmrRun(curTime);
 }
 
-function tmrSingleConvert(number){
+//used to stop timer
+function tmrStop(){
+    tmrRunning = false;
+}
+
+function tmrStartOrStop(){
+
+    if(tmrRunning){
+        tmrStartStopBtn.textContent = "Start";
+        tmrAddOptions.classList.remove("hide");
+        tmrSubOptions.classList.remove("hide");
+        tmrStop();
+    }
+    else{
+        tmrStartStopBtn.textContent = "Stop";
+        tmrAddOptions.classList.add("hide");
+        tmrSubOptions.classList.add("hide");
+        tmrStart();
+    }
+}
+
+function tmrRun(prevTime)
+{
+    if(tmrRunning){
+        var curTime = Date.now();
+        tmrAmount -= curTime - prevTime;
+        tmrConvertFromMS(tmrAmount);
+        tmrHrDisplay.textContent = tmrSingleDigitConvert(tmrTimes[3]);
+        tmrMinDisplay.textContent = tmrSingleDigitConvert(tmrTimes[2]);
+        tmrSecDisplay.textContent = tmrSingleDigitConvert(tmrTimes[1]);
+
+        if(tmrAmount <= 0){
+            tmrFinished();
+        }
+        else{
+            tmrLoop = setTimeout(function(){tmrRun(curTime);}, 100);
+        }
+    }
+}
+
+function tmrReset(){
+    //reset Values
+    clearTimeout(tmrLoop);
+    tmrHrDisplay.textContent = "00";
+    tmrMinDisplay.textContent = "00";
+    tmrSecDisplay.textContent = "00";
+    tmrRunning = false;
+    tmrAmount = 0;
+    tmrTimes = [0,0,0,0];
+    tmrStartStopBtn.textContent = "Start";
+    tmrAddOptions.classList.remove("hide");
+    tmrSubOptions.classList.remove("hide");
+}
+
+function tmrFinished(){
+    tmrReset();
+    
+    //hide timer buttons
+    tmrAlarmOff.classList.remove("remove");
+    tmrAlarmOff.classList.add("show");
+    tmrOptions.classList.remove("show");
+    tmrOptions.classList.add("remove");
+
+    //play audio
+    tmrAudio.play();
+}
+
+function tmrAlarmReset(){
+    //stop and reset audio clip
+    tmrAudio.pause();
+    tmrAudio.currentTime = 0;
+
+    //remove off btn and add timer btns
+    tmrAlarmOff.classList.remove("show");
+    tmrAlarmOff.classList.add("remove");
+    tmrOptions.classList.remove("remove");
+    tmrOptions.classList.add("show");
+
+    //show tmr add/sub
+    tmrAddOptions.classList.remove("hide");
+    tmrSubOptions.classList.remove("hide");
+}
+
+//Conversion functions
+function tmrConvertToMS(){
+    tmrAmount = Number(tmrHrDisplay.textContent)*60*60*1000;
+    tmrAmount += Number(tmrMinDisplay.textContent)*60*1000;
+    tmrAmount += Number(tmrSecDisplay.textContent)*1000;
+}
+
+function tmrConvertFromMS(msAmnt){
+    var unconverted = msAmnt;
+    for(var i = 0; i<tmrMaxs.length; i++){
+        tmrTimes[i] = unconverted%tmrMaxs[i];
+        unconverted = Math.floor(unconverted/tmrMaxs[i]);
+    }
+}
+
+function tmrSingleDigitConvert(number){
     if(number < 10){
         number = "0" + number;
     }
     return number;
 }
 
-function tmrFinished(){
-    tmrValueReset();
-    tmrStateToggle();
-    tmrAudio.play();
-    tmrAlarmOff.classList.remove("remove");
-    tmrAlarmOff.classList.add("show");
-    tmrOptions.classList.remove("show");
-    tmrOptions.classList.add("remove");
-}
-
-function tmrAlarmReset(){
-    tmrAudio.pause();
-    tmrAudio.currentTime = 0;
-    tmrSubAddToggle();
-
-    tmrAlarmOff.classList.remove("show");
-    tmrAlarmOff.classList.add("remove");
-    tmrOptions.classList.remove("remove");
-    tmrOptions.classList.add("show");
-}
-
-function tmrToggle(){
-    tmrStateToggle();
-    tmrSubAddToggle();
-    timer();
-}
-
-function tmrStateToggle(){
-    tmrState = !tmrState;
-}
-
-function tmrSubAddToggle(){
-    if(tmrState){
-        tmrAddOptions.classList.add("hide");
-        tmrSubOptions.classList.add("hide");
-        tmrStartStopBtn.textContent = "Stop";
-    }
-    else{
-        tmrAddOptions.classList.remove("hide");
-        tmrSubOptions.classList.remove("hide");
-        tmrStartStopBtn.textContent = "Start";
-    }
-}
-
+//click or hold functions
 function clickOrHold(funcName, display, arg1){
     funcName(display, arg1);
     tmrCreateInterval(funcName, display, arg1);
@@ -149,14 +168,7 @@ function tmrSub(display, max){
     display.textContent = value;
 }
 
-function tmrValueReset(){
-    if(!tmrState){
-        tmrHrDisplay.textContent = "00";
-        tmrMinDisplay.textContent = "00";
-        tmrSecDisplay.textContent = "00"; 
-    }
-}
-
+//event listeners
 document.getElementById("tmrAddHr").addEventListener("mousedown", function(){clickOrHold(tmrAdd, tmrHrDisplay, 24)});
 document.getElementById("tmrSubHr").addEventListener("mousedown", function(){clickOrHold(tmrSub, tmrHrDisplay, 24)});
 document.getElementById("tmrAddMin").addEventListener("mousedown", function(){clickOrHold(tmrAdd, tmrMinDisplay, 59)});
@@ -164,7 +176,7 @@ document.getElementById("tmrSubMin").addEventListener("mousedown", function(){cl
 document.getElementById("tmrAddSec").addEventListener("mousedown", function(){clickOrHold(tmrAdd, tmrSecDisplay, 59)});
 document.getElementById("tmrSubSec").addEventListener("mousedown", function(){clickOrHold(tmrSub, tmrSecDisplay, 59)});
 
-document.getElementById("tmrState").addEventListener("click", tmrToggle);
-tmrResetBtn.addEventListener("click", tmrValueReset);
+document.getElementById("tmrState").addEventListener("click", tmrStartOrStop);
+tmrResetBtn.addEventListener("click", tmrReset);
 tmrAlarmOffBtn.addEventListener("click", tmrAlarmReset);
 document.addEventListener("mouseup", function(){release();});
