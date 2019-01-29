805c49ce75630acb755b60160477c183e9fe90de
James Thayer <jrthayer@users.noreply.github.com>
2019-01-28 20:14:33 -0800

    Convert timer js into class
    *Now needs a constructor
    -object variables are tied to the keyword this(scope) rather than location in code.
    *Functions are defined just by name
    *Event bindings require arrow functions to pass scope since this is tied to the object that the event is tied to otherwise

--- timer.js
+++ timer.js
@@ -1,182 +1,192 @@
-var tmrHrDisplay = document.getElementById("tmrHrs");
-var tmrMinDisplay = document.getElementById("tmrMins");
-var tmrSecDisplay = document.getElementById("tmrSecs");
-var tmrAudio = document.getElementById("tmrNoise");
-var tmrStartStopBtn = document.getElementById("tmrState");
-var tmrResetBtn = document.getElementById("tmrReset");
-var tmrOptions = document.getElementById("tmrOptions");
-var tmrAlarmOffBtn = document.getElementById("tmrAlarmOff");
-var tmrAddOptions = document.getElementById("tmrAddOptions");
-var tmrSubOptions = document.getElementById("tmrSubOptions");
+class Timer{
+    constructor(timer){
+        this.tmrHrDisplay = document.getElementById("tmrHrs");
+        this.tmrMinDisplay = document.getElementById("tmrMins");
+        this.tmrSecDisplay = document.getElementById("tmrSecs");
+        this.tmrAudio = document.getElementById("tmrNoise");
+        this.tmrStartStopBtn = document.getElementById("tmrState");
+        this.tmrResetBtn = document.getElementById("tmrReset");
+        this.tmrOptions = document.getElementById("tmrOptions");
+        this.tmrAlarmOffBtn = document.getElementById("tmrAlarmOff");
+        this.tmrAddOptions = document.getElementById("tmrAddOptions");
+        this.tmrSubOptions = document.getElementById("tmrSubOptions");
 
-var adjustRepeat = null;
-var tmrLoop;
-var tmrRunning = false;
-var tmrAmount = 0;
-var tmrMaxs = [1000, 60, 60, 24];
-var tmrTimes = [0,0,0,0];
+        this.adjustRepeat = null;
+        this.tmrLoop;
+        this.tmrRunning = false;
+        this.tmrAmount = 0;
+        this.tmrMaxs = [1000, 60, 60, 24];
+        this.tmrTimes = [0,0,0,0];
 
-//used when to start or continue timer
-function tmrStart(){
-    tmrRunning = true;
-    var curTime = Date.now();
-    tmrConvertToMS();
-    tmrRun(curTime);
-}
-
-//used to stop timer
-function tmrStop(){
-    tmrRunning = false;
-}
-
-function tmrStartOrStop(){
-
-    if(tmrRunning){
-        tmrStartStopBtn.textContent = "Start";
-        tmrAddOptions.classList.remove("hide");
-        tmrSubOptions.classList.remove("hide");
-        tmrStop();
+        //event listeners
+        document.getElementById("tmrAddHr").addEventListener("mousedown", () => this.clickOrHold(this.tmrAdd, this.tmrHrDisplay, 24));
+        document.getElementById("tmrSubHr").addEventListener("mousedown", () => this.clickOrHold(this.tmrSub, this.tmrHrDisplay, 24));
+        document.getElementById("tmrAddMin").addEventListener("mousedown", () => this.clickOrHold(this.tmrAdd, this.tmrMinDisplay, 59));
+        document.getElementById("tmrSubMin").addEventListener("mousedown", () => this.clickOrHold(this.tmrSub, this.tmrMinDisplay, 59));
+        document.getElementById("tmrAddSec").addEventListener("mousedown", () => this.clickOrHold(this.tmrAdd, this.tmrSecDisplay, 59));
+        document.getElementById("tmrSubSec").addEventListener("mousedown", () => this.clickOrHold(this.tmrSub, this.tmrSecDisplay, 59));
+        this.tmrStartStopBtn.addEventListener("click", () => this.tmrStartOrStop());
+        this.tmrResetBtn.addEventListener("click", () => this.tmrReset());
+        this.tmrAlarmOffBtn.addEventListener("click", () => this.tmrAlarmReset());
+        document.addEventListener("mouseup", () => this.release());
     }
-    else{
-        tmrStartStopBtn.textContent = "Stop";
-        tmrAddOptions.classList.add("hide");
-        tmrSubOptions.classList.add("hide");
-        tmrStart();
-    }
-}
 
-function tmrRun(prevTime)
-{
-    if(tmrRunning){
+    //used when to start or continue timer
+    tmrStart(){
+        this.tmrRunning = true;
         var curTime = Date.now();
-        tmrAmount -= curTime - prevTime;
-        tmrConvertFromMS(tmrAmount);
-        tmrHrDisplay.textContent = tmrSingleDigitConvert(tmrTimes[3]);
-        tmrMinDisplay.textContent = tmrSingleDigitConvert(tmrTimes[2]);
-        tmrSecDisplay.textContent = tmrSingleDigitConvert(tmrTimes[1]);
+        this.tmrConvertToMS();
+        this.tmrRun(curTime);
+    }
 
-        if(tmrAmount <= 0){
-            tmrFinished();
+    //used to stop timer
+    tmrStop(){
+        this.tmrRunning = false;
+    }
+
+    tmrStartOrStop(){
+        if(this.tmrRunning){
+            this.tmrStartStopBtn.textContent = "Start";
+            this.tmrAddOptions.classList.remove("hide");
+            this.tmrSubOptions.classList.remove("hide");
+            this.tmrStop();
         }
         else{
-            tmrLoop = setTimeout(function(){tmrRun(curTime);}, 100);
+            this.tmrStartStopBtn.textContent = "Stop";
+            this.tmrAddOptions.classList.add("hide");
+            this.tmrSubOptions.classList.add("hide");
+            this.tmrStart();
         }
     }
-}
 
-function tmrReset(){
-    //reset Values
-    clearTimeout(tmrLoop);
-    tmrHrDisplay.textContent = "00";
-    tmrMinDisplay.textContent = "00";
-    tmrSecDisplay.textContent = "00";
-    tmrRunning = false;
-    tmrAmount = 0;
-    tmrTimes = [0,0,0,0];
-    tmrStartStopBtn.textContent = "Start";
-    tmrAddOptions.classList.remove("hide");
-    tmrSubOptions.classList.remove("hide");
-}
+    tmrRun(prevTime)
+    {
+        if(this.tmrRunning){
+            var curTime = Date.now();
+            this.tmrAmount -= curTime - prevTime;
+            this.tmrConvertFromMS(this.tmrAmount);
+            this.tmrHrDisplay.textContent = this.tmrSingleDigitConvert(this.tmrTimes[3]);
+            this.tmrMinDisplay.textContent = this.tmrSingleDigitConvert(this.tmrTimes[2]);
+            this.tmrSecDisplay.textContent = this.tmrSingleDigitConvert(this.tmrTimes[1]);
 
-function tmrFinished(){
-    tmrReset();
-    
-    //hide timer buttons
-    tmrAlarmOff.classList.remove("remove");
-    tmrAlarmOff.classList.add("show");
-    tmrOptions.classList.remove("show");
-    tmrOptions.classList.add("remove");
+            if(this.tmrAmount <= 0){
+                this.tmrFinished();
+            }
+            else{
+                this.tmrLoop = setTimeout(() => this.tmrRun(curTime), 100);
+            }
+        }
+    }
 
-    //play audio
-    tmrAudio.play();
-}
+    tmrReset(){
+        if(this.tmrRunning === true){
+            this.tmrAddOptions.classList.remove("hide");
+            this.tmrSubOptions.classList.remove("hide"); 
+        }
 
-function tmrAlarmReset(){
-    //stop and reset audio clip
-    tmrAudio.pause();
-    tmrAudio.currentTime = 0;
+        //reset Values
+        clearTimeout(this.tmrLoop);
+        this.tmrHrDisplay.textContent = "00";
+        this.tmrMinDisplay.textContent = "00";
+        this.tmrSecDisplay.textContent = "00";
+        this.tmrRunning = false;
+        this.tmrAmount = 0;
+        this.tmrTimes = [0,0,0,0];
+        this.tmrStartStopBtn.textContent = "Start";
+    }
 
-    //remove off btn and add timer btns
-    tmrAlarmOff.classList.remove("show");
-    tmrAlarmOff.classList.add("remove");
-    tmrOptions.classList.remove("remove");
-    tmrOptions.classList.add("show");
+    tmrFinished(){
+        this.tmrRunning = false;
+        this.tmrReset();
+        
+        //hide timer buttons
+        this.tmrAlarmOffBtn.classList.remove("remove");
+        this.tmrAlarmOffBtn.classList.add("show");
+        this.tmrOptions.classList.remove("show");
+        this.tmrOptions.classList.add("remove");
 
-    //show tmr add/sub
-    tmrAddOptions.classList.remove("hide");
-    tmrSubOptions.classList.remove("hide");
-}
+        //play audio
+        this.tmrAudio.play();
+    }
 
-//Conversion functions
-function tmrConvertToMS(){
-    tmrAmount = Number(tmrHrDisplay.textContent)*60*60*1000;
-    tmrAmount += Number(tmrMinDisplay.textContent)*60*1000;
-    tmrAmount += Number(tmrSecDisplay.textContent)*1000;
-}
+    tmrAlarmReset(){
+        //stop and reset audio clip
+        this.tmrAudio.pause();
+        this.tmrAudio.currentTime = 0;
 
-function tmrConvertFromMS(msAmnt){
-    var unconverted = msAmnt;
-    for(var i = 0; i<tmrMaxs.length; i++){
-        tmrTimes[i] = unconverted%tmrMaxs[i];
-        unconverted = Math.floor(unconverted/tmrMaxs[i]);
+        //remove off btn and add timer btns
+        tmrAlarmOff.classList.remove("show");
+        tmrAlarmOff.classList.add("remove");
+        tmrOptions.classList.remove("remove");
+        tmrOptions.classList.add("show");
+
+        //show tmr add/sub
+        this.tmrAddOptions.classList.remove("hide");
+        this.tmrSubOptions.classList.remove("hide");
+    }
+
+    //Conversion functions
+    tmrConvertToMS(){
+        this.tmrAmount = Number(this.tmrHrDisplay.textContent)*60*60*1000;
+        this.tmrAmount += Number(this.tmrMinDisplay.textContent)*60*1000;
+        this.tmrAmount += Number(this.tmrSecDisplay.textContent)*1000;
+    }
+
+    tmrConvertFromMS(msAmnt){
+        var unconverted = msAmnt;
+        for(var i = 0; i<this.tmrMaxs.length; i++){
+            this.tmrTimes[i] = unconverted%this.tmrMaxs[i];
+            unconverted = Math.floor(unconverted/this.tmrMaxs[i]);
+        }
+    }
+
+    tmrSingleDigitConvert(number){
+        if(number < 10){
+            number = "0" + number;
+        }
+        return number;
+    }
+
+    //click or hold functions
+    clickOrHold(funcName, display, arg1){
+        funcName(display, arg1);
+        this.tmrCreateInterval(funcName, display, arg1);
+    }
+
+    tmrCreateInterval(funcName, display, arg1){
+        this.adjustRepeat = setInterval(function(){funcName(display, arg1)}, 125)
+    }
+
+    release(){
+        clearInterval(this.adjustRepeat);
+    }
+
+    tmrAdd(display, max){
+        var value = Number(display.textContent);
+        value += 1;
+        if(value < 10){
+            value = "0" + value;
+        }
+        else if(value > max){
+            value = "00";
+        }
+        display.textContent = value;
+    }
+
+    tmrSub(display, max){
+        var value = Number(display.textContent);
+        value -= 1;
+        if(value < 0){
+            value = max;
+        }
+        else if(value < 10){
+            value = "0" + value; 
+        }
+        
+        display.textContent = value;
     }
 }
 
-function tmrSingleDigitConvert(number){
-    if(number < 10){
-        number = "0" + number;
-    }
-    return number;
-}
+let test = new Timer(document.querySelector('.timer'));
 
-//click or hold functions
-function clickOrHold(funcName, display, arg1){
-    funcName(display, arg1);
-    tmrCreateInterval(funcName, display, arg1);
-}
 
-function tmrCreateInterval(funcName, display, arg1){
-    adjustRepeat = setInterval(function(){funcName(display, arg1)}, 125)
-}
-
-function release(){
-    clearInterval(adjustRepeat);
-}
-
-function tmrAdd(display, max){
-    var value = Number(display.textContent);
-    value += 1;
-    if(value < 10){
-        value = "0" + value;
-    }
-    else if(value > max){
-        value = "00";
-    }
-    display.textContent = value;
-}
-
-function tmrSub(display, max){
-    var value = Number(display.textContent);
-    value -= 1;
-    if(value < 0){
-        value = max;
-    }
-    else if(value < 10){
-        value = "0" + value; 
-    }
-    
-    display.textContent = value;
-}
-
-//event listeners
-document.getElementById("tmrAddHr").addEventListener("mousedown", function(){clickOrHold(tmrAdd, tmrHrDisplay, 24)});
-document.getElementById("tmrSubHr").addEventListener("mousedown", function(){clickOrHold(tmrSub, tmrHrDisplay, 24)});
-document.getElementById("tmrAddMin").addEventListener("mousedown", function(){clickOrHold(tmrAdd, tmrMinDisplay, 59)});
-document.getElementById("tmrSubMin").addEventListener("mousedown", function(){clickOrHold(tmrSub, tmrMinDisplay, 59)});
-document.getElementById("tmrAddSec").addEventListener("mousedown", function(){clickOrHold(tmrAdd, tmrSecDisplay, 59)});
-document.getElementById("tmrSubSec").addEventListener("mousedown", function(){clickOrHold(tmrSub, tmrSecDisplay, 59)});
-
-document.getElementById("tmrState").addEventListener("click", tmrStartOrStop);
-tmrResetBtn.addEventListener("click", tmrReset);
-tmrAlarmOffBtn.addEventListener("click", tmrAlarmReset);
-document.addEventListener("mouseup", function(){release();});
